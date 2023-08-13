b9725dc28d20a4db6565d84164a92daa346a216c
Oshioke-Salaki <salaki1902@gmail.com>
2023-08-13 21:57:37 +0100

    updated

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 function Search({ query, setQuery }) {
+  const inputEl = useRef(null)
+
+  useEffect(function(){
+    function callback(e){
+      if(e.code === 'Enter'){
+        if(document.activeElement === inputEl.current) return;
+        inputEl.current.focus()
+        setQuery('')
+      }
+    }
+
+    document.addEventListener('keydown', callback)
+    inputEl.current.focus()
+
+    return function(){
+      document.removeEventListener('keydown', callback)
+    }
+  }, [setQuery])
   return (
     <input
       className="search"
       type="text"
       placeholder="Search movies..."
       value={query}
-      onChange={(e) => setQuery(e.target.value)} />
+      onChange={(e) => setQuery(e.target.value)} 
+      ref={inputEl}
+      />
   );
 }
 export default Search
